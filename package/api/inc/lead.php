b8ceebe60293c5be577c44037e46182faa069b2e
james exton <jay.exton@live.co.uk>
2020-09-11 14:40:47 +0100

    initial

---
+++ package/api/inc/lead.php
@@ -0,0 +1,113 @@
+<?php 
+
+namespace project;
+
+// require __DIR__ . '/../vendor/autoload.php'; 
+
+class lead{
+
+
+
+    function write_to_waypoint($lead, $business){
+
+        $param_string = "";
+        foreach($lead as $key => $value){ 
+            $param = $key;
+            if($key == "name"){ $param = "fullname"; }
+            $param_string = $param_string . "&".$param ."=". $value;
+        }
+ 
+        foreach($business as $key => $value){ 
+            $param = $key; 
+            $param_string = $param_string . "&".$param ."=". $value;
+        }
+ 
+        $ch = curl_init();  
+        
+        $url = "https://opop.waypointsoftware.io/capture.php?xAuthentication=be9bf09f2a0a40c850452d4d3c4bc0b9" . $param_string; 
+        // echo $url;
+        curl_setopt($ch, CURLOPT_URL, $url);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $waypoint_response = curl_exec($ch);
+        curl_close($ch);  
+        
+        return $waypoint_response;  
+        die();
+    }
+
+
+
+
+
+    public function enquiry_data($enquiry_id, $company, $industry, $location, $role, $salary){
+        $db = new db;
+        $stmt = $db->db->prepare("INSERT INTO  enquiry_prep (enquiry_id, company, industry , location  , role  , salary ) VALUES (:enquiry_id, :company, :industry, :location, :role, :salary )");
+        $stmt->bindValue(":enquiry_id", $enquiry_id);
+        $stmt->bindValue(":company", $company);
+        $stmt->bindValue(":industry", $industry); 
+        $stmt->bindValue(":location", $location);   
+        $stmt->bindValue(":role", $role);   
+        $stmt->bindValue(":salary", $salary);   
+        $checkResult = $stmt->execute();
+        $lastid = $db->getLast();
+        if($checkResult){  
+            $db = null;
+            return $lastid ;
+        }else{            
+            $db = null;
+            return false;
+        } 
+
+    }
+    public function save($name, $email, $telephone, $url){ 
+
+        $date = date("d-m-Y H:i");
+        $unix_date = time();
+
+        $db = new db;
+        $stmt = $db->db->prepare("INSERT INTO  leads (name, email , telephone,  url, date, unix_date  ) VALUES (:name, :email, :phone, :url, :date , :unix_date )");
+        $stmt->bindValue(":name", $name);
+        $stmt->bindValue(":email", $email); 
+        $stmt->bindValue(":phone", $telephone);   
+        $stmt->bindValue(":url", $url);
+        $stmt->bindValue(":date", $date);  
+        $stmt->bindValue(":unix_date", $unix_date);  
+         
+        $checkResult = $stmt->execute();
+        $lastid = $db->getLast();
+        if($checkResult){  
+            $db = null;
+            return $lastid ;
+        }else{            
+            $db = null;
+            return false;
+        } 
+	}       
+	
+    function log_visit($ip,  $referrer, $page ){
+        $db = new db;
+         
+        $d = time();
+        $stmt = $db->db->prepare("INSERT INTO  tracking (ip, referrer, page, date ) VALUES (:ip, :r, :p, :d)");
+
+        $stmt->bindValue(":ip", $ip);
+        $stmt->bindValue(":r", $referrer);    
+        $stmt->bindValue(":p", $page);
+        $stmt->bindValue(":d", $d);    
+
+        $checkResult = $stmt->execute();
+        $lastid = $db->getLast();
+        
+        if($checkResult){  
+            $db = null;
+            return $lastid ;
+        }else{            
+            $db = null;
+            return false;
+        }  
+    }
+       
+
+}
---
+++ partials/footer.php
@@ -0,0 +1,47 @@
+<?php ?>
+<footer class="footer" role="contentinfo">  
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div  class="footer-text">
+                    <p class="footer-title">Our Policies</p>
+                    <!-- <p>
+                        Whether you’re a candidate looking for a new role, or a client looking to recruit, call us on <a class="thick" href="+447597851249">+44 7597 851249</a> to speak to a member of the team – or <a class="thick" href="mailto:david.taylor@tailored-personnel.com">send us an email</a>
+                    </p> -->
+                </div>
+                <div class="social-links">
+                    <a>
+                        <img />
+                    </a>
+                </div>
+                <div class="footer-links">
+                    <ul> 
+                        <!-- <li><a href="/terms">Terms and Conditions</a></li> -->
+                        <li><a target="_blank" href="/docs/privacy.pdf">Privacy Policy</a></li> 
+                        <li><a target="_blank"  href="/docs/cookies.pdf">Cookie Policy</a></li> 
+                        <li><a target="_blank"  href="/docs/terms.pdf">Terms & Conditions</a></li> 
+                    </ul>
+                </div> 
+                <!-- <div> <p>Copyright Tailored Personnel © 2020</p> </div> -->
+            </div>
+
+            <div class="col-12 offset-md-1 col-md-5">
+                <div class="footer-right">
+                    <div class="footer-logo">
+                        <img src="/assets/img/logo.png" alt="Tailored Personnel Logo" />
+                    </div>
+                    <p>
+                        <a href="https://opopmedia.co.uk">Developed by - <span class="black thin"><span class="thick">OPOP</span>Media</span></a>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="loader-modal">
+    <div class="loader-container">
+        <p>Submitting Your Enquiry.</p>
+        <div class="loader"></div>
+    </div>
+</div>
