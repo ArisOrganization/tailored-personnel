fe83e8062007cb6a3b68f011e7e0fca1d25b1e39
Pedro <Pedro@opopmedia.co.uk>
2020-09-23 12:40:30 +0100

    change structure

--- recruit.php
+++ recruit.php
@@ -40,6 +40,7 @@
                 <section class='hero'>
                     <div class='container'>
                         <div class='row'>
+                            <div class="col-12 col-lg-6"></div>
                             <div class='col-12 col-lg-6'>
                                 <div class="hero-header" data-aos="fade-up" data-aos-duration="800">
                                     <div class="hero-header-content">
---
+++ steps.php
@@ -0,0 +1,45 @@
+<?php 
+    session_start();  
+?>
+<!doctype html>
+<html lang="en-GB" class="no-js">
+    <head>
+        <meta charset="UTF-8">
+        <title>Tailored Personel</title>
+        <meta name="description" content="A totally bespoke and tailored talent acquisition service.">   
+        <?php include "partials/head.php" ?>
+ 
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '212428966840264');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=212428966840264&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Facebook Pixel Code -->
+        <script>
+            fbq('track', 'ViewContent');
+        </script>
+    </head>
+    <body class="body"> 
+        <!-- header -->
+        <?php include "partials/header.php" ?>
+        <!-- /header -->
+        <main role="main">
+
+        </main> 
+        <!-- footer -->
+        <?php include "partials/footer.php" ?>
+        <!-- /footer -->  
+        <script src="/dist/js/main.js"></script>  
+    </body>  
+</html>
