c5c106542580644b4781bc6b3fb900cabf8e1764
aris <aris@opopmedia.co.uk>
2021-12-02 14:30:37 +0000

    add changes

---
+++ package/terms.php
@@ -0,0 +1,267 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en-GB" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tailored Personel</title>
+    <meta name="description" content="A totally bespoke and tailored talent acquisition service.">
+    <?php include "partials/head.php" ?>
+
+
+    <style>
+        .body-privacy {
+            margin-top: 80px;
+            display: flex;
+            flex-direction: column;
+
+            justify-content: center;
+            align-items: center;
+        }
+
+        .txct {
+            text-align: left;
+
+        }
+    </style>
+    <!-- Facebook Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '212428966840264');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=212428966840264&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
+    <script>
+        fbq('track', 'ViewContent');
+    </script>
+</head>
+
+<body class="body">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ8XVZ8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- header -->
+    <?php include "partials/header.php" ?>
+    <!-- /header -->
+    <main class="body-privacy px-5 pb-5" role="main">
+
+        <h3 class="mt-2">
+            Terms and Conditions of Business
+        </h3>
+        <p class="mt-2 txct">
+            These Terms and Conditions of Business are between Tailored Personnel (UK) Limited (herein after
+            called the AGENCY) and the Employer Client (hereinafter called the CLIENT) and are deemed to be
+            accepted by the Client by virtue of an interview or the engagement of a candidate introduced by the
+            Agency. The Client shall notify the Agency within 3 hours if the Candidate has applied directly to the
+            Client or if they have already been introduced by another recruitment agency or recruitment
+            business, otherwise the Introduction will be deemed to have been made by the Agency. No variation
+            in price will be accepted unless the Client has obtained the Agency’s prior written approval.
+        </p>
+        <p class="mt-2">
+            SUPPLY OF PERMANENT STAFF
+        </p>
+        <p class="mt-2">
+            1. In these Terms of Business, the following definitions apply:
+        </p>
+        <p class="mt-2">
+            “Engagement” means the engagement, employment, or use of a candidate on a permanent basis,
+            whether under a contract of service or for services, under an agency, licence, franchise or
+            partnership agreement or any other engagement or where applicable if the applicant has become
+            incorporated, the engagement of that limited company, or become a partner, the engagement of
+            that partnership;
+        </p>
+        <p class="mt-2">
+            “Introduction” means the identification of a candidate on a permanent basis to the Client by one or
+            more of the following methods: (a) verbally; (b) the submission of written details; (c) the applicant
+            attending an interview;
+            “Regulations” means the Conduct of Employment Agencies & Employment Businesses Regulations
+            2003;
+        </p>
+        <p class="mt-2">
+            2. Client agrees:
+        </p>
+        <p class="mt-2">
+            a) To notify the Agency immediately an Engagement is accepted; and
+        </p>
+        <p class="mt-2">
+            b) To pay the fee of the Agency within 5 days of the date of the invoice
+        </p>
+        <p class="mt-2">
+            c) To notify the Agency of an invoice dispute within 5 days of the date of the invoice, all disputes
+            must be reported in writing to the Agency. Failure to dispute within this time period will result in the
+            Agency rejecting any dispute.
+        </p>
+        <p class="mt-2">
+            The Agency reserves the right to charge interest accruing on a daily basis, on all outstanding invoices
+            at a rate of 5% above the Bank of England base rate per annum.
+        </p>
+        <p class="mt-2">
+            3. For the purposes of the Regulations the Agency is engaged by the Client as an Employment
+            Agency as defined in the Regulations.
+        </p>
+
+        <p class="mt-2">
+            4. The fee payable to the Agency by the Client for the introduction of a candidate is based on the
+            remuneration which the applicant is entitled to earn during the first 12 months of his/her
+            engagement by the Client. If the candidate is paid on a weekly basis this will be charged by timing
+            the wages by 52 weeks. Remuneration includes all taxable emoluments payable to or receivable by
+            the candidate for services rendered to or on behalf of the Client, this includes car allowances. Where
+            the candidate is offered a company vehicle, £6000 will be added to the salary for the purpose of
+            calculating the fee.
+        </p>
+        <p class="mt-2">
+            The fee per candidate is calculated at 8% of the basic annual salary and are subject to VAT
+        </p>
+
+        <p class="mt-2">
+            Retainer
+        </p>
+        <p class="mt-2">
+            A fixed price per month for an agreed number of positions to be filled on either a contingency
+            and/or associate basis. Price on enquiry and to be agreed in writing between the parties.
+        </p>
+        <p class="mt-2">
+            5. Should the Client fail to notify or disclose to the Agency of an Engagement of an introduced
+            candidate then the Agency reserves the right to charge the Client the fee calculated in accordance
+            with paragraph 4 which shall be payable immediately. If the Client fails to inform the Agency of the
+            remuneration payable to an client following an Engagement then the introduction fee payable under
+            this clause will be the higher of maximum remuneration specified by the Client for the vacancy or
+            the candidate’s maximum salary expectation specified in the CV supplied to the Client by the
+            Agency. No refund on an introduction fee will be made to the Client in the event of subsequent
+            termination of such engagements.
+        </p>
+        <p class="mt-2">
+            6. If, after an offer of Engagement has been made to the candidate the Client decides for any reason
+            to withdraw it, the Client shall be liable to pay the Agency a minimum fee of 5% of annual
+            remuneration.
+        </p>
+        <p class="mt-2">
+            7. Introductions are confidential. The passing on of an introduction to another employer, associate
+            or any affiliated party, which results in an Engagement renders the Client liable to payment of the
+            Agency’s introduction fee as set out in Paragraph 4 with no entitlement to a rebate or refund.
+        </p>
+        <p class="mt-2">
+            8. An introduction fee calculated in accordance with Paragraph 4 of these terms will be charged in
+            relation to any applicant engaged by the client that has been introduced by or through the Agency,
+            even if the introduction has been made indirectly. The fee is payable where the Agency have
+            submitted the application of the candidate even if the applicant subsequently applies to the Client
+            by any other source within a 12-month period from the date of the Agency’s Introduction. In such
+            cases no replacement candidates will be covered under the guarantee set out in paragraph 12.
+        </p>
+        <p class="mt-2">
+            9. Should the Client or any subsidiary, holding company or subsidiary of such holding company
+            subsequently re-engage the candidate within the period of 12 calendar months from the date of
+            termination of an Engagement or withdrawal of an offer, a further fee calculated in accordance with
+            paragraph 4 becomes payable, with no entitlement to any replacement candidates.
+        </p>
+        <p class="mt-2">
+            10. The Client shall make all payments due under the Contract without any deduction whether by
+            way of set off, counterclaim, discount, abatement or otherwise unless the Client has a valid court
+            order requiring an amount equal to such deduction to be paid by the Company to the Client. No
+            payment shall be deemed to be received until the Agency has received cleared funds.
+        </p>
+        <p class="mt-2">
+            11. As directors you guarantee to pay personally any fees (including disbursements) for services
+            provided to the company that the company is unable to pay. This clause shall become effective in
+            the event of a receiver or liquidator being appointed to the company or the company otherwise
+            being wound-up.
+        </p>
+        <p class="mt-2">
+            12. Should a candidate be found to be unsuitable for the role within the 4-week probation period
+            the Agency will supply a further candidate free of charge
+            The following conditions must be met in order for the Client to qualify for a replacement
+        </p>
+        <p class="mt-3">
+            a) Client must notify Employment Agency that the Candidate’s employment has ended within 7 days
+            of the employment ending or within 7 days of notice being given to end the employment (whichever
+            is earlier) together with a reason for the premature end of the employment;
+        </p>
+        <p class="mt-2">
+            b) Employment Agency’s invoice for the fee must have been paid within the payment terms in
+            accordance with paragraphs 2 and 4;
+        </p>
+        <p class="mt-2">
+            c) The Candidate’s employment is not terminated by reason of redundancy or re-organisation or
+            change in strategy of Client;
+        </p>
+        <p class="mt-2">
+            d) The Candidate’s employment is not terminated by reason of poor performance prior to the
+            completion of any induction or training period;
+        </p>
+        <p class="mt-2">
+            e) If the Candidate’s employment is terminated by reason of misconduct, replacement is only due
+            where such misconduct was reasonably foreseeable by Employment Agency;
+        </p>
+        <p class="mt-2">
+            f) The Candidate did not leave the employment because he/she reasonably believed that the nature
+            of the actual work was substantially different from the information Client provided prior to the
+            Candidate’s acceptance of the employment;
+        </p>
+        g) The Candidate did not leave the employment as a result of discrimination or other acts against the Candidate;
+        <p class="mt-2">
+            13. If any trial period is offered to the candidate by the client this will still be viewed as an offer of
+            employment and will be charged as stated in paragraph 4.
+        </p>
+        <p class="mt-2">
+            14. The Agency endeavours to ensure the suitability of the candidate introduced to the Client. The
+            Client shall notwithstanding satisfy him/herself as to the suitability of any candidate and shall take
+            up any references provided by the applicant and/or investigations into the medical history of any
+            candidate and satisfy any medical or other requirements or qualifications required by law.
+        </p>
+        <p class="mt-2">
+
+            15. The Agency shall not be liable under any circumstances for any loss, damage, expense, delay or
+            loss of profit or liability suffered or incurred by the client arising from or in any way connected to the
+            company seeking an applicant for the client, the introduction of any applicant by the company, the
+            engagement of any applicant, or by any reason arising out of the lack of skills, qualifications,
+            experience, integrity or suitability of the applicant by the client, or otherwise by the reason of any
+            act, omission, delay, default whether wilful, reckless, negligent or otherwise on the part of any
+            applicant.
+        </p>
+        <p class="mt-2">
+            16. The client shall ensure that any work carried out by any applicant is duly covered under any
+            insurance policies that it may have in place.
+        </p>
+        <p class="mt-2">
+
+            17. The client shall indemnify the company against any and all claims, demands, actions, proceedings
+            and liabilities of any third party against the company which arise out of or in connection with or have
+            been caused by either the failure or refusal by the client to engage any applicant , or otherwise by
+            any act, omission, delay or otherwise caused by the applicant whilst engaged by the client
+        </p>
+
+
+
+
+
+    </main>
+    <!-- footer -->
+    <?php include "partials/footer.php" ?>
+    <!-- /footer -->
+    <script src=" /dist/js/main.js">
+    </script>
+</body>
+
+</html>
---
+++ privacy.php
@@ -0,0 +1,212 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en-GB" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tailored Personel</title>
+    <meta name="description" content="A totally bespoke and tailored talent acquisition service.">
+    <?php include "partials/head.php" ?>
+
+
+    <style>
+        .body-privacy {
+            margin-top: 80px;
+            display: flex;
+            flex-direction: column;
+
+            justify-content: center;
+            align-items: center;
+        }
+
+        .txct {
+            text-align: left;
+
+        }
+    </style>
+    <!-- Facebook Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '212428966840264');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=212428966840264&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
+    <script>
+        fbq('track', 'ViewContent');
+    </script>
+</head>
+
+<body class="body">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ8XVZ8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- header -->
+    <?php include "partials/header.php" ?>
+    <!-- /header -->
+    <main class="body-privacy px-5 pb-5" role="main">
+
+        <h2>Privacy Policy</h2>
+        <h3 class="my-3">Who we are</h3>
+
+        <p class="my-2"> Our website address is: http://tailored-personnel.com.</p>
+        <h3 class="my-2">What personal data we collect and why we
+            collect it</h3>
+        <h5 class="my-2">Comments</h5>
+
+        <p class="mt-2 txct">
+            When visitors leave comments on the site we collect the data shown in the
+            comments form, and also the visitor’s IP address and browser user agent
+            string to help spam detection.
+        </p>
+        <p class="mt-2 txct">
+            An anonymised string created from your email address (also called a hash)
+            may be provided to the Gravatar service to see if you are using it. The
+            Gravatar service Privacy Policy is available here:
+            https://automattic.com/privacy/. After approval of your comment, your
+            profile picture is visible to the public in the context of your comment.
+        </p>
+
+        <h5 class="mt-2">Media</h5>
+
+        <p class="mt-2 txct">
+            If you upload images to the website, you should avoid uploading images
+            with embedded location data (EXIF GPS) included. Visitors to the website
+            can download and extract any location data from images on the website.
+        </p>
+        <h5 class="mt-2">Contact forms</h5>
+        <h5 class="mt-2">
+            Cookies
+        </h5>
+
+        <p class="mt-2 txct">
+            If you leave a comment on our site you may opt in to saving your name,
+            email address and website in cookies. These are for your convenience so
+            that you do not have to fill in your details again when you leave another
+            comment. These cookies will last for one year.
+        </p>
+        <p class="mt-2 txct">
+            If you visit our login page, we will set a temporary cookie to determine if
+            your browser accepts cookies. This cookie contains no personal data and
+            is discarded when you close your browser.
+        </p>
+        <p class="mt-2 txct">
+            When you log in, we will also set up several cookies to save your login
+            information and your screen display choices. Login cookies last for two
+            days, and screen options cookies last for a year. If you select “Remember
+            Me”, your login will persist for two weeks. If you log out of your account, the
+            login cookies will be removed.
+        </p>
+        <p class="mt-2 txct">
+            If you edit or publish an article, an additional cookie will be saved in your
+            browser. This cookie includes no personal data and simply indicates the
+            post ID of the article you just edited. It expires after 1 day.
+        </p>
+
+        <h5 class="mt-2">
+            Embedded content from other websites
+        </h5>
+        <p class="mt-2 txct">
+            Articles on this site may include embedded content (e.g. videos, images,
+            articles, etc.). Embedded content from other websites behaves in the exact
+            same way as if the visitor has visited the other website.
+        </p>
+        <p class="mt-2 txct">
+            These websites may collect data about you, use cookies, embed additional
+            third-party tracking, and monitor your interaction with that embedded
+            content, including tracking your interaction with the embedded content if
+            you have an account and are logged in to that website.
+        </p>
+        <h5 class="mt-2">
+            Analytics
+        </h5>
+        <h3 class="mt-3">
+            Who we share your data with
+        </h3>
+        <h3 class="mt-3">
+            How long we retain your data
+        </h3>
+        <p class="mt-2 txct">
+            If you leave a comment, the comment and its metadata are retained
+            indefinitely. This is so we can recognise and approve any follow-up
+            comments automatically instead of holding them in a moderation queue.
+        </p>
+        <p class="mt-2 txct">
+            For users that register on our website (if any), we also store the personal
+            information they provide in their user profile. All users can see, edit, or
+            delete their personal information at any time (except they cannot change
+            their username). Website administrators can also see and edit that
+            information.
+        </p>
+        <h3 class="mt-2">
+            What rights you have over your data
+        </h3>
+        <p class="mt-2 txct">
+            If you have an account on this site, or have left comments, you can request
+            to receive an exported file of the personal data we hold about you,
+            including any data you have provided to us. You can also request that we
+            erase any personal data we hold about you. This does not include any data
+            we are obliged to keep for administrative, legal, or security purposes.
+        </p>
+        <h3 class="mt-2">
+            Where we send your data
+        </h3>
+        <p class="mt-2 txct">
+            Visitor comments may be checked through an automated spam detection
+            service.
+        </p>
+        <h3 class="mt-2">
+            Your contact information
+        </h3>
+        <h3 class="mt-2">
+            Additional information
+        </h3>
+        <h3 class="mt-2">
+            How we protect your data
+        </h3>
+        <h3 class="mt-2">
+            What data breach procedures we have in place
+        </h3>
+        <h3 class="mt-3">
+            What third parties we receive data from
+        </h3>
+        <h3 class="mt-2">
+            What automated decision making and/or profiling we do
+            with user data
+        </h3>
+        <h3 class="mt-2">
+            Industry regulatory disclosure requirements
+        </h3>
+
+
+
+    </main>
+    <!-- footer -->
+    <?php include "partials/footer.php" ?>
+    <!-- /footer -->
+    <script src=" /dist/js/main.js">
+    </script>
+</body>
+
+</html>
